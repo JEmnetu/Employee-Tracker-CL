7489b6ae52df5ffabb43b34088ec10728a8a609a
JHabte <j.habtema@gmail.com>
2020-01-21 01:09:46 -0500

    added sql seed and basic queries

--- app.js
+++ app.js
@@ -1,1 +1,77 @@
 let mysql = require('mysql');
+let cTable = require('console.table');
+let inquirer = require('inquirer');
+
+let connection = mysql.createConnection({
+
+    host: 'localhost',
+    port: '3306',
+    user: 'root',
+    password: '',
+    database: 'employees_db'
+
+});
+
+let queryDept = () => {
+    // Render Department Table
+    connection.query("SELECT * FROM department;", (err, data) => {
+        if (err) {
+            console.log('SQL Error => ' + err.stack);
+        }
+        console.table(data);
+
+    });
+}
+
+let queryRole = () => {
+    // Render Role Table
+    connection.query("SELECT * FROM role;", (err, data) => {
+        if (err) {
+            console.log("SQL Error => " + err.stack);
+        }
+        console.table(data);
+    });
+}
+
+let queryEmployee = () => {
+
+    // Render Employee Table
+    connection.query("SELECT * FROM employee;", (err, data) => {
+        if (err) {
+            console.log("SQL Error => " + err.stack);
+        }
+        console.table(data);
+    });
+
+
+}
+
+
+
+connection.connect((err) => {
+    if (err) {
+        console.log("Big ole error => " + err.stack);
+        return;
+    }
+
+    console.log('Connection to SQL established as ID ' + connection.threadId);
+});
+
+//================== SQL Queries ==================
+
+// queryDept();
+
+inquirer.prompt([{
+    name: 'action',
+    message: 'What would you like to do?',
+    type: 'list',
+    choices: ['Display all Departments', 'Display all Roles', 'Display all Employees']
+}]).then((response) => {
+    if (response.action === 'Display all Departments') {
+        queryDept();
+    } else if (response.action === 'Display all Roles') {
+        queryRole();
+    } else if (response.action === 'Display all Employees') {
+        queryEmployee();
+    }
+});
--- schema/seeds.sql
+++ schema/seeds.sql
@@ -1,6 +1,8 @@
-DROP DATABASE IF EXISTS employees_db;
-
 CREATE DATABASE employees_db;
+
+USE employees_db;
+
+-- --------------Create Tables----------------
 
 CREATE TABLE department(
 id INTEGER NOT NULL AUTO_INCREMENT,
@@ -30,3 +32,30 @@ manager_id INTEGER,
 PRIMARY KEY (id)
 
 );
+
+
+--  -----------------------Populate Tables-----------------------------
+
+INSERT INTO department (name)VALUES 
+('Administration'), 
+('Human Resources'), 
+('Accounting'), 
+('IT'), 
+('Payroll');
+
+ INSERT INTO role (title, salary, department_id) VALUES
+ ('CEO', 100000.00, 1), 
+ ('Biller', 45000.00, 3), 
+ ('CTO', 80000, 4), 
+ ('Payroll Manager',77000.00, 5), 
+ ('Developer', 67000.00, 4), 
+ ('Recruiter', 49000.00, 2);
+
+  INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES
+  ('John', 'Smith', 1,null),
+ ('Allen', 'Brown', 3, null), 
+ ('Brandon', 'Hill', 2, 4), 
+ ('Ashley', 'Penn', 2, 4), 
+ ('Lisa', 'Dunn', 4, null), 
+ ('Jacob', 'H', 5, 3), 
+ ('Neb', 'T', 6, 4);
