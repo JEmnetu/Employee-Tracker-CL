e95d5925bb5f6d678c5c0fcdd7811d5fcec15bcb
JHabte <j.habtema@gmail.com>
2020-01-24 01:50:40 -0500

    seperated schema and seeds, placed in db folder

---
+++ db/schema.sql
@@ -0,0 +1,34 @@
+CREATE DATABASE IF NOT EXISTS employees_db;
+
+USE employees_db;
+
+-- --------------Create Tables----------------
+
+CREATE TABLE department(
+id INTEGER NOT NULL AUTO_INCREMENT,
+name VARCHAR(30) NOT NULL,
+
+PRIMARY KEY(id)
+
+);
+
+CREATE TABLE role(
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER,
+
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employee(
+
+id INTEGER NOT NULL AUTO_INCREMENT,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+role_id INTEGER NOT NULL,
+manager_id INTEGER,
+
+PRIMARY KEY (id)
+
+);
